f8478d0b7f8840ec654434b27f4831f1046625f1
David Pitoniak <davidpitoniak32@gmail.com>
2025-02-12 21:42:19 -0500

    chore: setup commit lint hook

--- apps/adder-esm/src/main.spec.ts
+++ apps/adder-esm/src/main.spec.ts
@@ -1,9 +1,18 @@
-import { adder } from "./main.js";
+import { adder, adderCjs } from "./main.js";
 
 describe("adder-esm", () => {
 	it("add + add", () => {
 		// Arrange / Act
 		const result = adder(1, 1);
+		// Assert
+		expect(result).toEqual(6);
+	});
+});
+
+describe("adder-cjs", () => {
+	it("add + add", () => {
+		// Arrange / Act
+		const result = adderCjs(1, 1);
 		// Assert
 		expect(result).toEqual(4);
 	});
